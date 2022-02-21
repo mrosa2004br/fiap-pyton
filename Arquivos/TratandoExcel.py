17293267f1dd9c7facd72241b3435efc8ffe45bc
mrosa2004br <mrosa2004br@gmail.com>
2022-02-20 23:02:20 -0300

    Separar os arquivos em pastas por formato.

--- Arquivos/TratandoExcel.py
+++ Arquivos/TratandoExcel.py
@@ -1,12 +1,13 @@
 import pandas as pd
 
 listaMeses = ['janeiro','fevereiro','março','abril','maio','junho']
+filePath = "excel/"
 
 for mes in listaMeses:
 
     print(mes)
 
-    tabelaVendas = pd.read_excel( mes + '.xlsx', engine='openpyxl')
+    tabelaVendas = pd.read_excel( filePath + mes + '.xlsx', engine='openpyxl')
 
     print(tabelaVendas)
 
