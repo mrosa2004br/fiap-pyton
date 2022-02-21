a17efb8ce7a8d59750bf7ef5032057b257b5d243
mrosa2004br <mrosa2004br@gmail.com>
2022-02-20 22:40:10 -0300

    Ajuste na leitura dos arquivos xls e interar e todos os arquivos.

--- Arquivos/TratandoExcel.py
+++ Arquivos/TratandoExcel.py
@@ -1,16 +1,14 @@
 import pandas as pd
-import xlrd
 
 listaMeses = ['janeiro','fevereiro','março','abril','maio','junho']
 
 for mes in listaMeses:
+
     print(mes)
 
-tabelaVendas = xlrd.open_workbook("janeiro.xlsx")
+    tabelaVendas = pd.read_excel( mes + '.xlsx', engine='openpyxl')
 
-##tabelaVendas = pd.read_excel('janeiro.xlsx')
-
-print(tabelaVendas)
+    print(tabelaVendas)
 
 
 
